98276fb5da214496483836871be22e53f098bbaa
Jordan Garcia <jordangarcia@Jordans-MacBook-Pro.local>
2019-04-18 12:05:02 -0400

    articleViewer updates

--- src/js/gen_functions.js
+++ src/js/gen_functions.js
@@ -7,48 +7,10 @@ function change1()
 	var plus = '\u2606';
 	var minus = '\u2605';
 
-    var sb = document.getElementById("saveButton1");
+    var sb = document.getElementById("vote");
     if ( sb.innerText == plus ) 
     	sb.innerText = minus;
     else 
     	sb.innerText = plus;
 }
 
-//changes save button when clicked
-function change2()
-{
-	var plus = '\u2606';
-	var minus = '\u2605';
-
-    var sb = document.getElementById("saveButton2");
-    if ( sb.innerText == plus ) 
-    	sb.innerText = minus;
-    else 
-    	sb.innerText = plus;
-}
-
-//changes save button when clicked
-function change3()
-{
-	var plus = '\u2606';
-	var minus = '\u2605';
-	
-    var sb = document.getElementById("saveButton3");
-    if ( sb.innerText == plus ) 
-    	sb.innerText = minus;
-    else 
-    	sb.innerText = plus;
-}
-
-//changes save button when clicked
-function change4()
-{
-	var plus = '\u2606';
-	var minus = '\u2605';
-
-    var sb = document.getElementById("saveButton4");
-    if ( sb.innerText == plus ) 
-    	sb.innerText = minus;
-    else 
-    	sb.innerText = plus;
-}
